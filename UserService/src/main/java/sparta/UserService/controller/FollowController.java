263e0ead5be2e13fadc0d6d2476998689984dfe3
GwanHwiJ <gwan@jeong-gwanhwiui-MacBookPro.local>
2024-06-23 17:24:55 +0900

    fix: 토큰 id를 통한 follow기능 매개변수 변경

--- UserService/src/main/java/sparta/UserService/controller/FollowController.java
+++ UserService/src/main/java/sparta/UserService/controller/FollowController.java
@@ -1,8 +1,8 @@
 package sparta.UserService.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-import sparta.UserService.dto.FollowUserRequestDto;
 import sparta.UserService.dto.FollowUserResponseDto;
 import sparta.UserService.service.FollowService;
 
@@ -14,7 +14,11 @@ public class FollowController {
     private final FollowService followService;
 
     @PostMapping("/create-follow")
-    public FollowUserResponseDto followUser(@RequestBody FollowUserRequestDto requestDto) {
-       return followService.followUser(requestDto);
+    public FollowUserResponseDto followUser(HttpServletRequest request, @RequestParam(name = "followingId") Long followingId) {
+       return followService.followUser(getMemberId(request), followingId);
+    }
+
+    private Long getMemberId(HttpServletRequest request) {
+        return (Long) request.getAttribute("member_id");
     }
 }
--- UserService/src/main/java/sparta/UserService/service/FollowService.java
+++ UserService/src/main/java/sparta/UserService/service/FollowService.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import sparta.UserService.dto.FollowUserRequestDto;
 import sparta.UserService.dto.FollowUserResponseDto;
 import sparta.UserService.entity.Follow;
 import sparta.UserService.entity.Member;
@@ -25,18 +24,18 @@ public class FollowService {
 
     private final MemberRepository memberRepository;
 
-    public FollowUserResponseDto followUser(FollowUserRequestDto dto) {
-        if(dto.getFollowingId().equals(dto.getFollowerId())){
+    public FollowUserResponseDto followUser(Long followerId, Long followingId) {
+        if(followingId.equals(followerId)){
             throw new RuntimeException("자기 자신을 팔로우 할 수 없습니다.");
         }
 
-        Optional<Follow> checkFollow = followRepository.findByFollowingIdAndFollowerId(dto.getFollowingId(), dto.getFollowerId());
+        Optional<Follow> checkFollow = followRepository.findByFollowingIdAndFollowerId(followingId, followerId);
         if(checkFollow.isPresent()){
             throw new RuntimeException("이미 팔로우한 회원입니다.");
         }
 
-        Member follower = memberRepository.findById(dto.getFollowerId()).orElseThrow();
-        Member following = memberRepository.findById(dto.getFollowingId()).orElseThrow();
+        Member follower = memberRepository.findById(followerId).orElseThrow();
+        Member following = memberRepository.findById(followingId).orElseThrow();
         Follow follow = new Follow(follower, following);
         followRepository.save(follow);
 
