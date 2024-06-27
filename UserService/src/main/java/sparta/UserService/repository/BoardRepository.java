29b4d7b665665ee4bd616c8e4056ee504b6afc71
GwanHwiJ <gwan@jeong-gwanhwiui-MacBookPro.local>
2024-06-27 12:40:17 +0900

    msa형태로 분리

--- UserService/src/main/java/sparta/UserService/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package sparta.UserService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sparta.UserService.entity.Board;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-}
--- UserService/src/main/java/sparta/UserService/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package sparta.UserService.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sparta.UserService.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
